67032d6dd646cea820251313d77d8c5638dba407
masoncoding2-0 <masoncoding@outlook.com>
2024-10-17 12:36:57 -0500

    Añadiendo la sección de hero sin animaciones

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+      {/* IMAGE CONTAINER */}
+      <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
+        <Image src="/hero.png" alt="Hero" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
+        {/* TITLE */}
+        <h1 className="text-4xl font-bold md:text-6xl">Crafting Digital Experiences, Designing Tomorrow.</h1>
+        {/* DESC */}
+        <p className="md:text-xl">
+          Welcome to my digital portfolio, 
+          where innovation and creativity converge. 
+          With a keen eye for aestheric and a mastery of code, 
+          my portfolio showcase a diverse collection of projects 
+          in differents programming languages reflect my expertise 
+          and my commitment to excellence.
+        </p>
+        <div className='flex gap-4'>
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">Download My CV</button>
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View My Work</button>
+          <button className="p-4 rounded-lg ring-1 ring-black">Contact me</button>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Homepage;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import Image from "next/image";
 import { useState } from "react";
+import NavLink from "./navLink";
 
 const links = [
     { url: "/", title: "Home" },
@@ -15,15 +16,15 @@ const links = [
 const Navbar = () => {
     const [open, setOpen] = useState(false)
     return (
-        <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+        <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl'>
             {/* LINKS */}
-            <div className='hidden md:flex gap-4 w-1/4'>
+            <div className='hidden md:flex gap-4 w-1/3'>
                 {links.map(link => (
-                    <Link href={link.url} key={link.title}>{link.title}</Link>
+                    <NavLink link={link} key={link.title}/>
                 ))}
             </div>
             {/*LOGO*/}
-            <div className='md:hidden lg:flex w-1/4'>
+            <div className='md:hidden lg:flex xl:w-1/3 xl:justify-center'>
                 <Link
                     href="/"
                     className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -35,7 +36,7 @@ const Navbar = () => {
                 </Link>
             </div>
             {/* SOCIAL */}
-            <div className='hidden md:flex gap-4 w-1/4'>
+            <div className='hidden md:flex gap-4 w-1/3'>
                 <Link href="/">
                     <Image src="/github.png" alt="GitHub" width={24} height={24} />
                 </Link>
